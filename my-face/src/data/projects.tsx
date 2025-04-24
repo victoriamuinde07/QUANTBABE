5c8d43184fc6e516a412487fa5be5769a45ac930
victoriamuinde07 <victoriamuinde07@gmail.com>
2025-04-24 15:34:33 +0300

    updated

--- my-face/src/data/projects.tsx
+++ my-face/src/data/projects.tsx
@@ -17,15 +17,22 @@ export const projects: Project[] = [
     tags: ["HTML","CSS","sass","bootstrap"]
   },
   {
+    title: "Riaor Construction website",
+    description: "For Riaor system ltd",
+    link: "https://riaor-construction.vercel.app/",
+    tags: ["html","css","bootstrap"]
+
+  },
+  {
     title: "AI-Powered Complaint Management System",
     description: "This is a AI-powered customer complaint management system that allows customers to lodge in finacial complaints and the trained NLP model gives the right complaint categorization",
     // Consider updating this link if your portfolio has its own repo/URL
     link: "https://huggingface.co/spaces/d9lph8n/AI.Poweredcomplaint_management_system",
     tags: ["python","SQLAlchemy","Machine Learning Algorithms"]
-  }
+  },
   {
     title: "Customer-Churn predictor Project",
-    description: "The project focuses on predicting the likelyhood for customer to churn.By understanding the patterns,telcom compabies such as SyriaTel can proactively enhance customer retention strategies, improve customer satisfaction and reduce financial losses",
+    description: "The project focuses on predicting the likelyhood for customer to churn.By understanding the patterns,telcom companies such as SyriaTel can proactively enhance customer retention strategies, improve customer satisfaction and reduce financial losses",
     link: "https://github.com/victoriamuinde07/Data-science-project-SyrialTel-project.git",
     tags: ["Data analysis","Machine Learning"]
   }
